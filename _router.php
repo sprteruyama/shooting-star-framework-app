0d27ddcf669465f9f67b698c833e485dfbb5179c
sprteruyama <teruyama@springboard-inc.jp>
2022-04-18 20:33:49 +0900

    updated _router.php for static resource

--- _router.php
+++ _router.php
@@ -1,2 +1,7 @@
 <?php
+$resourceFilePath = __DIR__ . '/app/Public/' . $_SERVER['SCRIPT_NAME'];
+if (file_exists($resourceFilePath)) {
+    echo file_get_contents($resourceFilePath);
+    exit();
+}
 require 'app/Public/index.php';
