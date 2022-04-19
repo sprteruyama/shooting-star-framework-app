893fa1dde11c55b653a31ed3f010f7ea933ca322
sprteruyama <teruyama@springboard-inc.jp>
2022-04-19 18:47:44 +0900

    updated _router.php for static resource

--- _router.php
+++ _router.php
@@ -1,6 +1,25 @@
 <?php
 $resourceFilePath = __DIR__ . '/app/Public/' . $_SERVER['SCRIPT_NAME'];
+$resourceFilePath = preg_replace('/\?.+$/', '', $resourceFilePath);
 if (file_exists($resourceFilePath) && !is_dir($resourceFilePath)) {
+    if (preg_match('/\.(.+)$/', $resourceFilePath, $matched)) {
+        $ext = strtolower($matched[1]);
+        switch ($ext) {
+            case 'jpg':
+                header('Content-Type: image/jpeg');
+                break;
+            case 'png':
+            case 'gif':
+                header('Content-Type: image/' . $ext);
+                break;
+            case 'css':
+                header('Content-Type: text/css');
+                break;
+            case 'js':
+                header('Content-Type: text/javascript');
+                break;
+        }
+    }
     echo file_get_contents($resourceFilePath);
     exit();
 }
